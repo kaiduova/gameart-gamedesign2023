e022c6c9d5a7e3161609f580d941545c82e67e88
kaiduova <k.chang0320211@arts.ac.uk>
2023-05-01 02:54:30 +0100

    Added bounce projectile mechanic

---
+++ Assets/Scripts/DeflectingBlock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Collider2D))]
+public class DeflectingBlock : MonoBehaviour
+{
+    private void Start()
+    {
+        gameObject.layer = 13;
+    }
+}
